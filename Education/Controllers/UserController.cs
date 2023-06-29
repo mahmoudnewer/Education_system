30c6c7bba0fdf4d12f7a0344552cbb4a889eaf2d
Abdulrhman Elsayed <abdulrhmanelsayed6@gmail.com>
2023-06-30 01:04:58 +0200

    save changes

---
+++ Education/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using Education.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Education.ViewModel;
+using BCrypt.Net;
+using Education.Services;
+
+namespace Education.Controllers
+{
+    public class UserController : Controller
+    {
+        IUserService userrepository;
+        IInstructorService instructorRepository;
+        public UserController(IUserService _userrepository, IInstructorService _instructorRepository)
+        {
+            userrepository = _userrepository;
+            instructorRepository = _instructorRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(User_ViewModel user)
+        {
+            string hashedPassword =instructorRepository.GetAll().FirstOrDefault(i=>i.Email== user.Email).Password;
+
+            bool isPasswordCorrect = BCrypt.Net.BCrypt.Verify(user.Password, hashedPassword);
+
+            if (isPasswordCorrect)
+            {
+                Instructor instructor = userrepository.Get(user.Email, user.Password);
+                ClaimsIdentity identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, instructor.Id.ToString()));
+                identity.AddClaim(new Claim(ClaimTypes.Name, instructor.Name.ToString()));
+                identity.AddClaim(new Claim(ClaimTypes.Role, userrepository.GetRole(instructor.Id)));
+
+                ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                return RedirectToAction("index", "Home");
+            }
+          
+            return View(user);
+        }
+        [Authorize]
+        public  IActionResult Profile()
+        {
+            var userIdClaim = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+           
+                int Id = int.Parse(userIdClaim.Value);
+                Instructor instructor = instructorRepository.GetById(Id);
+                ProfileViewModel profile = new ProfileViewModel()
+                {
+                    Name = instructor.Name,
+                    Email = instructor.Email,
+                    Phone = instructor.Phone,
+                    image = instructor.image,
+                    Address = instructor.Address,
+                    Age=instructor.Age,
+                    Role=userrepository.GetRole(Id)
+                };
+                return View(profile);
+            
+
+        }
+
+        public IActionResult LogOut()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return View("index");
+        }
+
+    }
+}
---
+++ Education/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using Education.Models;
+
+namespace Education.Services
+{
+    public interface IUserService
+    {
+        bool Find(string email, string password);
+        Instructor Get(string email, string password);
+        string GetRole(int id);
+    }
+}
