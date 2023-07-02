6a1f30ebaaa2acc3eedaa96e30bbfbf46d45eab7
3bdelmoniemahmed11 <3bdelmoniemahmed11@gmail.com>
2023-07-03 01:32:35 +0300

    Last Modifcation

--- Education/Controllers/GradeController.cs
+++ Education/Controllers/GradeController.cs
@@ -5,6 +5,7 @@ using Education.ViewModel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
+using System.Globalization;
 
 namespace Education.Controllers
 {
@@ -95,6 +96,65 @@ namespace Education.Controllers
 
 
             return PartialView("_AllGradePartial", g);
+        }
+        public IActionResult GenerateReport()
+        {
+            return View();
+        }
+
+            [HttpPost]
+        public IActionResult GenerateReport(string selectedDate)
+        {
+            DateTime endDate = DateTime.ParseExact(selectedDate, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime startDate = new DateTime(endDate.Year, endDate.Month, 1);
+            DateTime inclusiveEndDate = endDate.AddDays(1);
+            var averageNewGrades = _GradeService.GetAll()
+                .Where(g => g.Date >= startDate && g.Date < inclusiveEndDate && g.Type == "New")
+                .GroupBy(g => g.student.Name)
+                .Select(g => new
+                {
+                    StudentName = g.Key,
+                    AverageGrade = g.Average(s => s.grade)
+                })
+                .ToList();
+
+            var averageOldGrades = _GradeService.GetAll()
+                .Where(g => g.Date >= startDate && g.Date < inclusiveEndDate && g.Type == "Old")
+                .GroupBy(g => g.student.Name)
+                .Select(g => new
+                {
+                    StudentName = g.Key,
+                    AverageGrade = g.Average(s => s.grade)
+                })
+                .ToList();
+
+            var attendanceDays = _GradeService.GetAll()
+                .Where(g => g.Date >= startDate && g.Date < inclusiveEndDate)
+                .GroupBy(g => g.student.Name)
+                .Select(g => new
+                {
+                    StudentName = g.Key,
+                    AttendanceDays = g.Select(g => g.Date.Date).Distinct().Count()
+                })
+                .ToList();
+
+            var reportData = averageNewGrades
+                .Join(averageOldGrades, n => n.StudentName, o => o.StudentName, (n, o) => new
+                {
+                    StudentName = n.StudentName,
+                    AverageNewGrade = n.AverageGrade,
+                    AverageOldGrade = o.AverageGrade
+                })
+                .Join(attendanceDays, grades => grades.StudentName, attendance => attendance.StudentName, (grades, attendance) => new
+                {
+                    StudentName = grades.StudentName,
+                    AverageNewGrade = Math.Round(grades.AverageNewGrade, 2), // Round the average grade to 2 decimal places
+                    AverageOldGrade = Math.Round(grades.AverageOldGrade, 2), // Round the average grade to 2 decimal places
+                    AttendanceDays = attendance.AttendanceDays
+                })
+                .OrderByDescending(data => data.AttendanceDays + data.AverageNewGrade + data.AverageOldGrade)
+                .ToList();
+            return View(reportData.Cast<dynamic>().ToList());
         }
 
     }
